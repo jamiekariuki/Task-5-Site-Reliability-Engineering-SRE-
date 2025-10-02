d80fa8279e0d58f7ba68fd563d2e199265454bc3
jamiekariuki <jamiekariuki18@gmail.com>
2025-10-02 14:21:24 +0300

    fix: fix frontend base url

--- frontend-react/src/api.js
+++ frontend-react/src/api.js
@@ -2,7 +2,10 @@ import axios from "axios"
 import { jwtDecode } from "jwt-decode"
 
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL
+//export const BASE_URL = import.meta.env.VITE_BASE_URL
+
+//testing
+export const BASE_URL= "http://localhost:8000"
 
 const api = axios.create({
     baseURL: BASE_URL

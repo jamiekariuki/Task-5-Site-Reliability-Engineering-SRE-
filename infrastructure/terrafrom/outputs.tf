7c0992654c1117ebf4ef8a9f108025b7f182c68b
jamiekariuki <jamiekariuki18@gmail.com>
2025-10-05 00:18:00 +0300

    fix: fix helm provider

--- infrastructure/terrafrom/outputs.tf
+++ infrastructure/terrafrom/outputs.tf
@@ -7,6 +7,19 @@ output "backend_repository_url" {
 }
 
 
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
 
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "EKS cluster CA certificate"
+  value       = module.eks.cluster_certificate_authority_data
+}
 
 
--- infrastructure/terrafrom/providers.tf
+++ infrastructure/terrafrom/providers.tf
@@ -1,28 +1,22 @@
-
 provider "aws" {
   region              = var.region
   allowed_account_ids = [var.aws_account_id]
 }
- 
 
-data "aws_eks_cluster" "main" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "main" {
+data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
+  kubernetes ={
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
-} 
+}

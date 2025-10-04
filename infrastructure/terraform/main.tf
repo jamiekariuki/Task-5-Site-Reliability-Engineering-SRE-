e4238c320b2e5796eb5c3655b59ce27d0638c4c6
jamiekariuki <jamiekariuki18@gmail.com>
2025-10-05 01:27:42 +0300

    fix: fix eks access

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -51,6 +51,7 @@ module "eks" {
   }
 
   endpoint_public_access  = true
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets

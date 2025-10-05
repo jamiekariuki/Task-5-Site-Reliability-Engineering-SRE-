8faa4ab58a6239c8629a70141e6723af43330b58
jamiekariuki <jamiekariuki18@gmail.com>
2025-10-05 20:32:22 +0300

    fix: update alertmanager chart

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -182,25 +182,30 @@ alertmanager:
   service:
     type: LoadBalancer
   alertmanagerSpec:
-    replicas: 1
-  config:
-    global:
-      resolve_timeout: 5m
-    route:
-      group_by: ['alertname']
-      group_wait: 30s
-      group_interval: 5m
-      repeat_interval: 1h
-      receiver: email-alert
-    receivers:
-      - name: email-alert
-        email_configs:
-          - to: "jamiekariuki18@gmail.com"
-            from: "jamiekariuki18@gmail.com"
-            smarthost: "smtp.gmail.com:587"
-            auth_username: "jamiekariuki18@gmail.com"
-            auth_identity: "jamiekariuki18@gmail.com"
-            auth_password: "demopassword"
+    replicas: 2
+    alertmanagerConfigSelector:
+      matchLabels:
+        release: monitoring
+    alertmanagerConfigMatcherStrategy:
+      type: None
+    config:
+      global:
+        resolve_timeout: 5m
+      route:
+        receiver: "email-alert"
+        group_by: ['alertname']
+        group_wait: 30s
+        group_interval: 5m
+        repeat_interval: 1h
+      receivers:
+        - name: "email-alert"
+          email_configs:
+            - to: "jamiekariuki18@gmail.com"
+              from: "jamiekariuki18@gmail.com"
+              smarthost: "smtp.gmail.com:587"
+              auth_username: "jamiekariuki18@gmail.com"
+              auth_identity: "jamiekariuki18@gmail.com"
+              auth_password: "demopassword"
 EOF
   ]
 

e0668fa7bb308d8504cb3b14694cb23014d69977
jamiekariuki <jamiekariuki18@gmail.com>
2025-10-05 23:51:01 +0300

    fix: fix alert manager

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -183,30 +183,25 @@ alertmanager:
     type: LoadBalancer
   alertmanagerSpec:
     replicas: 1
-    # Match label that exists on Alertmanager pods
-    alertmanagerConfigSelector:
-      matchLabels:
-        app.kubernetes.io/name: alertmanager
-    alertmanagerConfigMatcherStrategy:
-      type: None
-    config:
-      global:
-        resolve_timeout: 5m
-      route:
-        receiver: "email-alert"
-        group_by: ['alertname']
-        group_wait: 30s
-        group_interval: 5m
-        repeat_interval: 1h
-      receivers:
-        - name: "email-alert"
-          email_configs:
-            - to: "jamiekariuki18@gmail.com"
-              from: "jamiekariuki18@gmail.com"
-              smarthost: "smtp.gmail.com:587"
-              auth_username: "jamiekariuki18@gmail.com"
-              auth_identity: "jamiekariuki18@gmail.com"
-              auth_password: a3puYiBya3JrIGVpaGwgeWV0bQo=
+  additionalConfig: |
+    global:
+      resolve_timeout: 5m
+    route:
+      receiver: "email-alert"
+      group_by: ['alertname']
+      group_wait: 30s
+      group_interval: 5m
+      repeat_interval: 1h
+    receivers:
+      - name: "email-alert"
+        email_configs:
+          - to: "jamiekariuki18@gmail.com"
+            from: "jamiekariuki18@gmail.com"
+            smarthost: "smtp.gmail.com:587"
+            auth_username: "jamiekariuki18@gmail.com"
+            auth_identity: "jamiekariuki18@gmail.com"
+            auth_password: a3puYiBya3JrIGVpaGwgeWV0bQo=
+
 EOF
   ]
 
